82ef41867ed6534d3ec4e44584da40fa5a2ce18c
JoseFlores97 <pepefloressalas@gmail.com>
2023-01-26 14:08:08 -0600

    feat(criteria): export RelationType

--- src/relations/index.ts
+++ src/relations/index.ts
@@ -1,7 +1,14 @@
 import { RelationName } from './relation-name';
 import { RelationField } from './relation-field';
 import { RelationValue } from './relation-value';
-import { Relation } from './relation';
+import { Relation, RelationType } from './relation';
 import { Relations } from './relations';
 
-export { RelationName, RelationField, RelationValue, Relation, Relations };
+export {
+  RelationName,
+  RelationField,
+  RelationValue,
+  Relation,
+  RelationType,
+  Relations,
+};
